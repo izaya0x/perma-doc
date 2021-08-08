2104ab7ba3c779c4645a38fc5cd3d82ff593ce2f
izaya0x <izaya0x@protonmail.com>
2021-08-07 18:46:57 -0700

    Remove un-used boilerplate contracts

--- packages/contracts/src/abis.js
+++ packages/contracts/src/abis.js
@@ -1,10 +1,6 @@
-import greeterAbi from "../artifacts/contracts/Greeter.sol/Greeter.json";
-import registrationAbi from "../artifacts/contracts/Registration.sol/Registration.json";
 import documentAbi from "../artifacts/contracts/Document.sol/Document.json";
 
 const abis = {
-  greeter: greeterAbi.abi,
-  registration: registrationAbi.abi,
   document: documentAbi.abi,
 };
 
--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -1,6 +1,4 @@
 const addresses = {
-  greeter: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512",
-  registration: "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
   document: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
 };
 
